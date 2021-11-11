6b5f758e586b883a0afd55414bea9caa36a54de2
DmitryBulin <lostsayron@gmail.com>
2021-11-11 22:10:13 +0300

    Implemented data recognition and storing
    From each log entry saving time_t for request time

--- Lab3/main.c
+++ Lab3/main.c
@@ -6,6 +6,14 @@
 
 char check_arguments_validation(int argc, char** argv);
 void print_arguments_info();
+unsigned long int get_line_count(FILE* file);
+unsigned short get_month_value(char* month_buff);
+
+typedef struct error_statistic
+{
+    char* request_info;
+    unsigned int error_quantity;
+} error_statistic;
 
 unsigned long long int required_time_gap;
 
@@ -19,7 +27,58 @@ int main(int argc, char** argv)
     FILE *input_file, *output_file;
     input_file = fopen(argv[1], "r");
     output_file = fopen(argv[3], "w");
+    printf("Got valid arguments. Starting to perform.\n");
 
+    unsigned long int input_lines_count = get_line_count(input_file);
+    time_t *request_times = malloc(input_lines_count * sizeof(time_t));
+    printf("Lines in input file: %lu\n", input_lines_count);
+    printf("Memory allocated: %zu\n", input_lines_count * sizeof(time_t));
+
+    char line_buff[256];
+    int whitespaces_passed_number, line_index;
+    struct tm local_time;
+
+    for (int i = 0; i < input_lines_count; i++)
+    {
+        fgets(line_buff, sizeof(line_buff), input_file);
+        line_index = 0;
+        whitespaces_passed_number = 0;
+        while (line_index < 256 && whitespaces_passed_number != 3)
+        {
+            if (line_buff[line_index] == ' ')
+            {
+                whitespaces_passed_number++;
+            }
+            line_index++;
+        }
+        line_index++;
+
+        local_time.tm_mday = (line_buff[line_index] - '0') * 10 + (line_buff[line_index + 1] - '0');
+        line_index += 3;
+
+        char *month_buff = malloc(3 * sizeof(char));
+        strncpy(month_buff, &line_buff[line_index], 3);
+        line_index += 4;
+        local_time.tm_mon = get_month_value(month_buff);
+
+        local_time.tm_year = (line_buff[line_index] - '0') * 1000 + (line_buff[line_index + 1] - '0') * 100 + (line_buff[line_index + 2] - '0') * 10 + (line_buff[line_index + 3] - '0') - 1900;
+        line_index += 5;
+
+        local_time.tm_hour = (line_buff[line_index] - '0') * 10 + (line_buff[line_index + 1] - '0');
+        line_index += 3;
+
+        local_time.tm_min = (line_buff[line_index] - '0') * 10 + (line_buff[line_index + 1] - '0');
+        line_index += 3;
+
+        local_time.tm_sec = (line_buff[line_index] - '0') * 10 + (line_buff[line_index + 1] - '0');
+        line_index += 3;
+
+        request_times[i] = mktime(&local_time);
+
+        free(month_buff);
+    }
+
+    free(request_times);
     fclose(input_file);
     fclose(output_file);
     return 0;
@@ -54,6 +113,7 @@ char check_arguments_validation(int argc, char** argv)
 
     return 1;
 }
+
 void print_arguments_info()
 {
     printf("<------- Wrong application arguments ------->\n");
@@ -62,4 +122,51 @@ void print_arguments_info()
     printf("    input - input file name\n");
     printf("    time - required time in seconds (0, 18446744073709551615]\n");
     printf("    input - input file name\n");
+}
+
+unsigned long int get_line_count(FILE* file)
+{
+    long unsigned int line_count = 1;
+    char temp_char;
+
+    while (!feof(file))
+    {
+        temp_char = fgetc(file);
+        if (temp_char == '\n')
+        {
+            line_count++;
+        }
+    }
+    fseek(file, 0L, SEEK_SET);
+
+    return line_count;
+}
+
+unsigned short get_month_value(char* month_buff)
+{
+    if (strcmp(month_buff, "Jan") == 0)
+        return 0;
+    if (strcmp(month_buff, "Feb") == 0)
+        return 1;
+    if (strcmp(month_buff, "Mar") == 0)
+        return 2;
+    if (strcmp(month_buff, "Apr") == 0)
+        return 3;
+    if (strcmp(month_buff, "May") == 0)
+        return 4;
+    if (strcmp(month_buff, "Jun") == 0)
+        return 5;
+    if (strcmp(month_buff, "Jul") == 0)
+        return 6;
+    if (strcmp(month_buff, "Aug") == 0)
+        return 7;
+    if (strcmp(month_buff, "Sep") == 0)
+        return 8;
+    if (strcmp(month_buff, "Oct") == 0)
+        return 9;
+    if (strcmp(month_buff, "Nov") == 0)
+        return 10;
+    if (strcmp(month_buff, "Dec") == 0)
+        return 11;
+    return 0;
 }
