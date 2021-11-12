cc0e1bba559163729ec6ca106dc957985f6fdb95
DmitryBulin <lostsayron@gmail.com>
2021-11-12 21:50:00 +0300

    Implemented count of failed requests
    Every request with status 5xx is defined failed and saved by its request info

--- Lab3/main.c
+++ Lab3/main.c
@@ -7,15 +7,17 @@
 char check_arguments_validation(int argc, char** argv);
 void print_arguments_info();
 unsigned long int get_line_count(FILE* file);
+time_t parsed_time(char* time_buff);
 unsigned short get_month_value(char* month_buff);
 
-typedef struct error_statistic
+typedef struct request_statistic
 {
     char* request_info;
-    unsigned int error_quantity;
-} error_statistic;
+    unsigned int request_number;
+} request_statistic;
 
 unsigned long long int required_time_gap;
+const short MAX_LINE_LENGTH = 512;
 
 int main(int argc, char** argv)
 {
@@ -30,55 +32,70 @@ int main(int argc, char** argv)
     printf("Got valid arguments. Starting to perform.\n");
 
     unsigned long int input_lines_count = get_line_count(input_file);
-    time_t *request_times = malloc(input_lines_count * sizeof(time_t));
-    printf("Lines in input file: %lu\n", input_lines_count);
-    printf("Memory allocated: %zu\n", input_lines_count * sizeof(time_t));
+    time_t *request_times = calloc(0, sizeof(time_t));
+    request_statistic *failed_requests = calloc(0, sizeof(request_statistic));
+    unsigned long int total_statistic_entry = 0;
 
-    char line_buff[256];
-    int whitespaces_passed_number, line_index;
-    struct tm local_time;
+    char *line_buff = calloc(MAX_LINE_LENGTH, sizeof(char)), *time_buff = calloc(20, sizeof(char));
+    unsigned char temp_index, request_start_index, request_length;
+    unsigned short request_status;
+    unsigned long int total_failed_request_number = 0, current_line_count = 0;
 
-    for (int i = 0; i < input_lines_count; i++)
+    while (fgets(line_buff, MAX_LINE_LENGTH, input_file))
     {
-        fgets(line_buff, sizeof(line_buff), input_file);
-        line_index = 0;
-        whitespaces_passed_number = 0;
-        while (line_index < 256 && whitespaces_passed_number != 3)
+        temp_index = 0;
+        while (line_buff[++temp_index] != '[');
+        strncpy(time_buff, line_buff + temp_index, 20);
+        request_times = realloc(request_times, (current_line_count + 1) * sizeof(time_t));
+        request_times[current_line_count] = parsed_time(time_buff);
+
+        while (line_buff[++temp_index - 1] != '"');
+        request_start_index = temp_index;
+        while (line_buff[++temp_index + 1] != '"');
+        request_length = temp_index - request_start_index + 1;
+        char *request_buff = calloc(request_length, sizeof(char) + 1);
+        strncpy(request_buff, line_buff + request_start_index, request_length);
+        request_buff[request_length] = '\0';
+
+        temp_index += 3;
+        request_status = (line_buff[temp_index] - '0') * 100 + (line_buff[temp_index + 1] - '0') * 10 + (line_buff[temp_index + 2] - '0');
+        if (request_status / 100 == 5)
         {
-            if (line_buff[line_index] == ' ')
+            total_failed_request_number++;
+            char found_simular_request = 0;
+            for (temp_index = 0; temp_index < total_statistic_entry; temp_index++)
             {
-                whitespaces_passed_number++;
+                if (strcmp(failed_requests[temp_index].request_info, request_buff) == 0)
+                {
+                    found_simular_request = 1;
+                    failed_requests[temp_index].request_number++;
+                    break;
+                }
             }
-            line_index++;
+            if (found_simular_request == 0)
+            {
+                failed_requests = realloc(failed_requests, (total_statistic_entry + 1) * sizeof(request_statistic));
+                failed_requests[total_statistic_entry].request_info = calloc(request_length + 1, sizeof(char));
+                strncpy(failed_requests[total_statistic_entry].request_info, request_buff, request_length + 1);
+                failed_requests[total_statistic_entry].request_number = 1;
+                total_statistic_entry++;
+            }
         }
-        line_index++;
 
-        local_time.tm_mday = (line_buff[line_index] - '0') * 10 + (line_buff[line_index + 1] - '0');
-        line_index += 3;
-
-        char *month_buff = malloc(3 * sizeof(char));
-        strncpy(month_buff, &line_buff[line_index], 3);
-        line_index += 4;
-        local_time.tm_mon = get_month_value(month_buff);
-
-        local_time.tm_year = (line_buff[line_index] - '0') * 1000 + (line_buff[line_index + 1] - '0') * 100 + (line_buff[line_index + 2] - '0') * 10 + (line_buff[line_index + 3] - '0') - 1900;
-        line_index += 5;
-
-        local_time.tm_hour = (line_buff[line_index] - '0') * 10 + (line_buff[line_index + 1] - '0');
-        line_index += 3;
-
-        local_time.tm_min = (line_buff[line_index] - '0') * 10 + (line_buff[line_index + 1] - '0');
-        line_index += 3;
-
-        local_time.tm_sec = (line_buff[line_index] - '0') * 10 + (line_buff[line_index + 1] - '0');
-        line_index += 3;
-
-        request_times[i] = mktime(&local_time);
-
-        free(month_buff);
+        free(request_buff);
+        current_line_count++;
     }
+    free(time_buff);
+    free(line_buff);
 
-    free(request_times);
+    fprintf(output_file, "Total failed requests: %d\n", total_failed_request_number);
+    fprintf(output_file, "Percentage of failed requests: %f%%\n", ((double)total_failed_request_number)/((double)current_line_count) * 100);
+    for (temp_index = 0; temp_index < total_statistic_entry; temp_index++)
+    {
+        fprintf(output_file, "%s => %d times\n", failed_requests[temp_index].request_info, failed_requests[temp_index].request_number);
+    }
+    printf("Result put in file %s\n", argv[3]);
+    free(failed_requests);
     fclose(input_file);
     fclose(output_file);
     return 0;
@@ -169,4 +186,21 @@ unsigned short get_month_value(char* month_buff)
     if (strcmp(month_buff, "Dec") == 0)
         return 11;
     return 0;
+}
+
+time_t parsed_time(char* time_buff)
+{
+    struct tm result_time;
+    result_time.tm_mday = (time_buff[0] - '0') * 10 + (time_buff[1] - '0');
+    result_time.tm_year = (time_buff[7] - '0') * 1000 + (time_buff[8] - '0') * 100 + (time_buff[9] - '0') * 10 + (time_buff[10] - '0') - 1900;
+    result_time.tm_hour = (time_buff[12] - '0') * 10 + (time_buff[13] - '0');
+    result_time.tm_min = (time_buff[15] - '0') * 10 + (time_buff[16] - '0');
+    result_time.tm_sec = (time_buff[18] - '0') * 10 + (time_buff[19] - '0');
+
+    char *month_buff = malloc(3 * sizeof(char));
+    strncpy(month_buff, time_buff + 3, 3);
+    result_time.tm_mon = get_month_value(month_buff);
+    free(month_buff);
+
+    return mktime(&result_time);
 }
